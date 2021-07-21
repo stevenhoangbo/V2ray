af4853b8b5289239b9620657335eed98c22bd458
hoangbo <80437625+stevenhoangbo@users.noreply.github.com>
2021-07-21 17:04:58 +0800

    Update master.sh

--- master.sh
+++ master.sh
@@ -3,4 +3,5 @@ apt-get update -y
 apt-get install curl -y
 apt-get install nginx -y
 apt-get install vim -y
+bash <(curl -L -s https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
 done
