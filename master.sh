c8b9ba742f7780a3232f0956f9beacc1cde1e0ba
hoangbo <80437625+stevenhoangbo@users.noreply.github.com>
2021-07-21 17:24:56 +0800

    Update master.sh

--- master.sh
+++ master.sh
@@ -3,5 +3,10 @@ apt-get update -y
 apt-get install curl -y
 apt-get install nginx -y
 apt-get install vim -y
+apt-get install lrzsz -y
 bash <(curl -L -s https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
+echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+sysctl -p
+systemctl enable v2ray
 done
