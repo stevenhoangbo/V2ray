90f45a29cb3e3819e33d2c8c06d905c337ef764d
hoangbo <80437625+stevenhoangbo@users.noreply.github.com>
2021-08-16 11:17:49 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,12 +1,13 @@
+#!/bin/bash
 server_port="server_port"
 local_port="local_port"
 password="password"
 method="method"
 
-
+cat>>config.json<<EOF
 "server":"127.0.0.1",
     "server_port":19342,
-    "local_port":1080,
+    "local_port":"1080",
     "local_address":"127.0.0.1",
     "password":"chinano1",
     "method": "aes-256-cfb",
