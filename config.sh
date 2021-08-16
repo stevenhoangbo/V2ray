dbc8c594b6068b6f9578f495dbba03465d358a9c
hoangbo <80437625+stevenhoangbo@users.noreply.github.com>
2021-08-16 10:51:22 +0800

    config,sh

---
+++ config.sh
@@ -0,0 +1,14 @@
+server_port="server_port"
+local_port="local_port"
+password="password"
+method="method"
+
+
+"server":"127.0.0.1",
+    "server_port":19342,
+    "local_port":1080,
+    "local_address":"127.0.0.1",
+    "password":"chinano1",
+    "method": "aes-256-cfb",
+    "timeout":600
+
