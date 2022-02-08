ec61d7957ead109f0e3981d8541bbbacd4c34ae4
hoangbo <80437625+stevenhoangbo@users.noreply.github.com>
2022-02-08 13:51:17 +0800

    Update master.sh

--- master.sh
+++ master.sh
@@ -2,7 +2,7 @@
 apt-get install nginx -y
 apt-get install vim -y
 apt-get install lrzsz -y
-bash <(curl -L -s https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
+bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh) --version v4.34.0
 echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
 sysctl -p
